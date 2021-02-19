d08b36d97eaedeac1c3ff34aeec9f26d32cbd7e5
FC5570 <68158483+FC5570@users.noreply.github.com>
2021-02-19 23:11:42 +0530

    intents

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Client = require('./src/client')
 const config = require('./config.json')
 
 const client = new Client({
+    ws: { intents: ['GUILD_MEMBERS'] },
     ownerID: config.ownerID,
     disableMentions: "everyone"
 })
